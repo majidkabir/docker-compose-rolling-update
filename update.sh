bfb97834188ef6d2ea8288f829341755c9c78985
Majid Ghafouri <majidkabir@gmail.com>
2025-08-14 09:57:21 +0200

    Run status checks concurrently

--- update.sh
+++ update.sh
@@ -2,4 +2,6 @@
 
 docker compose up -d --scale service1=2 --scale service2=2 --no-recreate
 
-./check_status.sh service1 & ./check_status.sh service2
+./check_status.sh service1 &
+./check_status.sh service2 &
+wait
